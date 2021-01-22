fc612a8dd8759a0c523dade5c75020a3c5d12961
shenzhuowei <1015575170@qq.com>
2021-01-22 12:17:53 +0800

    Signed-off-by: shenzhuowei <1015575170@qq.com>

--- two/controller/userCtrl.js
+++ two/controller/userCtrl.js
@@ -1,18 +1,20 @@
 const stuDao=require('../dao/app1Dao')
 module.exports={ 
     login(req,resp){
-          var sql="SELECT * FROM user;";
+          var sql="SELECT * FROM user";
           let founded = false;
+          console.log(req.body.userName,req.body.password)
           stuDao.getStuDao(sql,[],function(err,data){
               for (let user of data) {
                   if (user.userName === req.body.userName) {   
                    if ( user.password === req.body.password) {
                     founded = true;
-                    break;
+                  break;
                  }
-              break;
-              }
-              
+            
+                 break; 
+                 }
+           
           }
               console.log(founded);
                 if (founded) {
@@ -24,33 +26,42 @@ module.exports={
                 resp.end();
              })
     },
-    checks : (req,resp) => {//查一群
-      console.log(req.query);
+    checks(req,resp) {//查一群
+      //console.log(req.query);
       stuDao.getStuDao("SELECT * FROM user;",[],function(err,data){
           if(data.length>0){
               let queryData = JSON.stringify(data);
               console.log(queryData)
               resp.send(queryData);
           }
-           resp.end();
       })
   },
-  
-  delete : (req,resp) => {//删除
-    let id = req.body.id;
+  checks1(req,resp) {//查一群
+     var id = req.params['id'];  
+     console.log(id)
+    stuDao.getStuDao("SELECT * FROM user WHERE id=?",[id],function(err,data){
+        if(data.length>0){
+            let queryData = JSON.stringify(data);
+            console.log(queryData)
+            resp.send(queryData);
+        }
+    })
+},
+  delete (req,resp) {//删除
+    var id = req.body.id;
     console.log(id);
     stuDao.getStuDao("DELETE FROM user WHERE id = ?;",[id],function(err,data){
-        console.log(err);
-        console.log(data);
+        //console.log(err);
+        //console.log(data);
         resp.send({ succ:true });
-         resp.end();
   })
 },
     update(req,resp){
       let id = req.body.id;
+      let userName = req.body.userName;
       let password = req.body.password;
-          var sql='UPDATE user SET password = ? WHERE id = ?';
-          stuDao.getStuDao(sql,[password,id],function(err,result){
+          var sql='UPDATE user SET userName = ?,password = ? WHERE id = ?';
+          stuDao.getStuDao(sql,[userName,password,id],function(err,result){
             if(err){
              // console.log('[SELECT ERROR] - ',err.message);
               return;
@@ -58,6 +69,21 @@ module.exports={
         resp.send({ succ: true });
           resp.end();
           })
+    },
+    add(req,resp){
+      let id = req.body.id;
+      let userName = req.body.userName;
+      let password = req.body.password;
+          var sql='INSERT INTO user (id,userName,password) VALUES (?,?,?)';
+          stuDao.getStuDao(sql,[id,userName,password],function(err,result){
+            if(err){
+             // console.log('[SELECT ERROR] - ',err.message);
+              return;
+            }
+          resp.send({ succ: true });
+          resp.end();
+          })
     }
 }
+
 
